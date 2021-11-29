a924b069c0255cb713e8e0a42f9f442556b45a87
holonso <d1mo@live.com>
2021-11-29 19:51:12 +0900

    Добавьте файлы проекта.

---
+++ SDA2035_6HW/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDA2035_6HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение из слов разделенных пробелами");
+            string sentence = Console.ReadLine();
+            char[] probel = new char[] { ' ' };
+            string[] words = sentence.Split(probel, StringSplitOptions.RemoveEmptyEntries);
+            int maxcharN = 0;
+            string maxchar = "";
+            foreach (string s in words)
+            {
+                if (s.Length > maxcharN)
+                {
+                    maxcharN = s.Length;
+                    maxchar = s;
+                }
+            }
+            Console.WriteLine("Самое длинное слово в строке - {0}", maxchar);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ p06ex02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p06ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение из слов разделенных пробелами");
+            string sentence = Console.ReadLine();
+            string rwdSentence = new string(sentence.Reverse().ToArray());
+            if (sentence == rwdSentence)
+                Console.WriteLine("Предложение является палиндромом");
+            else
+                Console.WriteLine("Предложение не является палиндромом");
+            Console.ReadKey();
+        }
+    }
+}
